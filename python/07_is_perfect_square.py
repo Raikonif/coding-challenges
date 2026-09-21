42b2ffe77cdec29f45fa7c9ed18547cb0dde230b
Raikonif <raikonif@gmail.com>
2026-09-20 20:52:48 -0400

    feat: 07_is_perfect_squary.py done.

--- python/07_is_perfect_square.py
+++ python/07_is_perfect_square.py
@@ -11,5 +11,15 @@ Fuente: https://coding-challenges.dev/problems/python-es-cuadrado-perfecto
 
 
 def is_perfect_square(n: int) -> bool:
-    # Escribe tu solución aquí
+    if n >= 0:
+        for i in range(n + 1):
+            if i ** 2 == n:
+                return True
+            if i ** 2 > n:
+                break
     return False
+
+
+print(is_perfect_square(9))
+print(is_perfect_square(5))
+print(is_perfect_square(10))
